c57b2e20933a7470a651897b521853c86d91a30f
厉威 <13855761478@163.com>
2019-06-10 16:27:36 +0800

    TODOViewController完成delegate和dateSource方法

---
+++ TODO/TODOViewController.swift
@@ -0,0 +1,50 @@
+//
+//  TODOListViewController.swift
+//  TODO
+//
+//  Created by 厉威 on 2019/6/10.
+//  Copyright © 2019 厉威. All rights reserved.
+//
+
+import UIKit
+
+class TODOViewController: UITableViewController {
+    
+    let itemsArray = ["购买水杯","吃药","修改密码"]
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    // MARK: - Table view data source
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return itemsArray.count
+    }
+    
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cellID", for: indexPath)
+        cell.textLabel?.text = itemsArray[indexPath.row]
+        
+        return cell
+    }
+    
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print(indexPath.row)
+        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .none
+        }else{
+            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+        }
+        
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+    }
+    
+}
+
