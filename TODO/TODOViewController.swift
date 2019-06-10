f1af0525cd2c37b344bf9174ee5e2321fa303580
厉威 <13855761478@163.com>
2019-06-10 17:07:42 +0800

    添加新项目功能完成

--- TODO/TODOViewController.swift
+++ TODO/TODOViewController.swift
@@ -10,13 +10,42 @@ import UIKit
 
 class TODOViewController: UITableViewController {
     
-    let itemsArray = ["购买水杯","吃药","修改密码"]
+    var itemsArray = ["购买水杯","吃药","修改密码"]
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
     }
+    
+    // MARK: - IBAction
+    
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        
+        var textField = UITextField()
+        
+        
+        //创建弹窗控制器
+        let alert = UIAlertController(title: "添加一个新的TODO项目", message: "", preferredStyle: .alert)
+        
+        //创建动作
+        let action = UIAlertAction(title: "添加项目", style:.default) { (action) in
+            //用户单击添加项目按钮以后执行的代码
+            self.itemsArray.append(textField.text!)
+            self.tableView.reloadData()
+        }
+        
+        alert.addTextField { (alerttextField) in
+            alerttextField.placeholder = "创建一个新项目"
+            //让textField指向alertTextField，因为出了闭包，alertTextField不存在
+            textField = alerttextField
+        }
+        
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+        
+    }
+    
     
     // MARK: - Table view data source
     
